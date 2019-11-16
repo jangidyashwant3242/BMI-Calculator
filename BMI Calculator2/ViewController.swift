66338c795aee8544d71daf8c137509a2657177aa
w3OnDemand <w3ondemand@w3OnDemands-MacBook-Air.local>
2019-11-16 18:32:02 +0530

    Initial Commit

---
+++ BMI Calculator2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator2
+//
+//  Created by w3OnDemand on 16/11/19.
+//  Copyright © 2019 w3OnDemand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
